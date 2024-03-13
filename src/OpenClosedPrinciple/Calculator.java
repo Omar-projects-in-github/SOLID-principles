53799198f5da6d2b07742f10a3bc2a58bfafc6f6
Omar-projects-in-github <polat.omar2002@gmail.com>
2024-03-13 13:32:25 +0500

    Single Responsibility and Open Closed principles were implemented

---
+++ src/OpenClosedPrinciple/Calculator.java
@@ -0,0 +1,12 @@
+package OpenClosedPrinciple;
+
+import java.security.InvalidParameterException;
+
+public class Calculator {
+    public void calculate(CalculatorOperation operation) {
+        if (operation == null)
+            throw new InvalidParameterException("Can not perform operation");
+
+        operation.perform();
+    }
+}
---
+++ src/OpenClosedPrinciple/CalculatorOperation.java
@@ -0,0 +1,5 @@
+package OpenClosedPrinciple;
+
+public interface CalculatorOperation {
+    void perform();
+}
